74d21d12a1d087479ad662ca63b835a8dd60bd22
Vladislav Urykov <vladislavurykov29@gmail.com>
2022-12-28 14:31:54 +0300

    password validation + bootstrap styles

--- src/layouts/Login.jsx
+++ src/layouts/Login.jsx
@@ -12,10 +12,19 @@ const Login = () => {
 
   const validatorConfig = {
     email: {
-      isRequired: { message: "Электронная почта обязательна для заполнения" }
+      isRequired: { message: "Электронная почта обязательна для заполнения" },
+      isEmail: { message: "Электронная почта введена неверно" }
     },
     password: {
-      isRequired: { message: "Пароль обязателен для заполнения" }
+      isRequired: { message: "Пароль обязателен для заполнения" },
+      isCapitalSymbol: {
+        message: "Пароль должен содержать хотя бы одну заглавную букву"
+      },
+      isContainDigit: { message: "Пароль должен содержать хотя бы одну цифру" },
+      min: {
+        value: 8,
+        message: `Пароль должен состоять минимум из 8 символов`
+      }
     }
   };
 
@@ -24,6 +33,8 @@ const Login = () => {
     setErrors(errors);
     return Object.keys(errors).length === 0;
   };
+
+  const isValid = Object.keys(errors).length === 0;
 
   const handleChange = ({ target }) => {
     setData((prev) => ({ ...prev, [target.name]: target.value }));
@@ -37,24 +48,37 @@ const Login = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <TextField
-        label="Email"
-        name="email"
-        value={data.email}
-        onChange={handleChange}
-        error={errors.email}
-      />
-      <TextField
-        label="Password"
-        type="password"
-        name="password"
-        value={data.password}
-        onChange={handleChange}
-        error={errors.password}
-      />
-      <button type="submit">Submit</button>
-    </form>
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-md-6 offset-md-3 p-4">
+          <h3 className="mb-4">Login</h3>
+          <form onSubmit={handleSubmit}>
+            <TextField
+              label="Email"
+              name="email"
+              value={data.email}
+              onChange={handleChange}
+              error={errors.email}
+            />
+            <TextField
+              label="Password"
+              type="password"
+              name="password"
+              value={data.password}
+              onChange={handleChange}
+              error={errors.password}
+            />
+            <button
+              type="submit"
+              disabled={!isValid}
+              className="btn btn-primary w-100 mx-auto"
+            >
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 };
 
